4c6972f608046574122cdda807e21dfc63218123
Yugabdh <yugabdhppashte@gmail.com>
2025-01-26 23:18:48 +0530

    Changed horizontal layout of service cards to vertical

--- components/services/services-grid.js
+++ components/services/services-grid.js
@@ -5,20 +5,26 @@ const ServicesGrid = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
       {services.map((service, idx) => (
-        <div key={idx} className="p-4">
-          <div className="flex items-center mb-4">
-            <Image 
+        <div key={idx} className="px-8 py-6 border rounded-xl hover:shadow-md">
+          <div className="mb-4">
+            <Image
               src={service.icon}
               alt={`${service.title} icon`}
               className="w-16 h-16 mr-4"
               height={512}
               width={512}
             />
-            <h3 className="text-2xl font-montserrat font-semibold">{service.title}</h3>
           </div>
-          <ul className="font-nunito flex flex-wrap">
+          <h3 className="text-2xl font-montserrat font-semibold mb-4">{service.title}</h3>
+          <ul className="font-nunito list-inside">
             {service.offering.map((item, idx) => (
-              <li key={idx} className="inline text-lg">{item}{" "}{idx < service.offering.length - 1 && <span className="text-lg mx-2">◆</span>}</li>
+              <li key={idx} className="flex items-center space-x-3 text-lg">
+                <svg className="shrink-0 w-3.5 h-3.5" aria-hidden="true" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="0.686295" y="12" width="16" height="16" transform="rotate(-45 0.686295 12)" fill="#EC2E90" />
+                </svg>
+
+                <span>{item}</span>
+              </li>
             ))}
           </ul>
         </div>
