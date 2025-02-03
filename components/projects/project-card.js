15e71859d50db621ec6e5238aa3ca684aaa68f6f
Yugabdh <yugabdhppashte@gmail.com>
2025-02-03 18:36:15 +0530

    Fixed issue of project card not clickable

--- components/projects/project-card.js
+++ components/projects/project-card.js
@@ -12,9 +12,9 @@ const ProjectCard = ({ src, alt, title, link }) => {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.1 }}
         transition={{ duration: 0.5, delay: 0.5, ease: "easeOut" }}
-        className="group relative overflow-hidden rounded-xl mt-4 shadow-md hover:shadow-lg">
+        className="group relative overflow-hidden">
         <Link
-          className="overflow-hidden rounded mt-4 shadow-md hover:shadow-lg"
+          className="overflow-hidden rounded shadow-sm hover:shadow-md relative inline-block group"
           href={`/work/${link}`}
           title="Click to know more about screen title">
           <Image
@@ -25,16 +25,16 @@ const ProjectCard = ({ src, alt, title, link }) => {
             loading="lazy"
             className="rounded h-full w-full"
           />
+          <div className="absolute rounded inset-0 bg-cerise-900/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center p-3">
+            <h3 className="text-dull-lavender-50 text-3xl font-montserrat font-semibold uppercase text-center">
+              {title}
+            </h3>
+            <p className="text-dull-lavender-50 text-lg font-montserrat uppercase mt-24">LEARN MORE</p>
+          </div>
+          <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white text-center md:hidden lg:hidden">
+            <h3 className="text-sm font-semibold p-2">{title}</h3>
+          </div>
         </Link>
-        <div className="absolute rounded inset-0 bg-cerise-900/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center p-3">
-          <h3 className="text-dull-lavender-50 text-3xl font-montserrat font-semibold uppercase text-center">
-            {title}
-          </h3>
-          <p className="text-dull-lavender-50 text-lg font-montserrat uppercase mt-24">LEARN MORE</p>
-        </div>
-        <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white text-center md:hidden lg:hidden">
-          <h3 className="text-sm font-semibold p-2">{title}</h3>
-        </div>
       </m.li>
     </LazyMotion>
   )
