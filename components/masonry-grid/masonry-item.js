85cc88600e1bd1bf1e4885871992abb4e4f0bee6
Yugabdh <yugabdhppashte@gmail.com>
2025-02-04 10:41:51 +0530

    Card overlay changes

--- components/masonry-grid/masonry-item.js
+++ components/masonry-grid/masonry-item.js
@@ -17,11 +17,14 @@ const MasonaryItem = ({ img }) => {
           loading="lazy"
           className="rounded h-full w-full"
         />
-        <div className="absolute rounded inset-0 bg-cerise-900/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center p-3">
+        <div className="absolute rounded inset-0 bg-cerise-900/60 opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center p-3">
           <h3 className="text-dull-lavender-50 text-3xl font-montserrat font-semibold uppercase text-center">
             {img.title}
           </h3>
-          <p className="text-dull-lavender-50 text-lg font-montserrat uppercase mt-24">LEARN MORE</p>
+          <p className="text-dull-lavender-50 text-lg font-montserrat uppercase mt-3">LEARN MORE</p>
+        </div>
+        <div className="absolute rounded inset-0 bg-cerise-900/60 opacity-0 max-md:group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center p-3">
+          <p className="text-dull-lavender-50 text-lg font-montserrat uppercase">LEARN MORE</p>
         </div>
         <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white text-center md:hidden lg:hidden">
           <h3 className="text-sm font-montserrat uppercase p-2">{img.title}</h3>
