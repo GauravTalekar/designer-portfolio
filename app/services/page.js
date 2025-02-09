c7019e6f9e249051dd4735f9fda285e8d40b066f
Yugabdh <yugabdhppashte@gmail.com>
2025-02-09 12:14:41 +0530

    +Added services page, added process config

---
+++ app/services/page.js
@@ -0,0 +1,140 @@
+import FadeInObserver from "@/components/fade-in-observer"
+import SectionComponent from "@/components/section-component"
+import SectionHeader from "@/components/section-header"
+import { services, processes } from "@/data/services"
+import { data } from "@/data/site-details"
+import Image from "next/image"
+
+export async function generateMetadata() {
+  return {
+    title: "Gaurav Talekar - Services",
+    description: "Discover services offered by Gaurav Talekar",
+    openGraph: {
+      title: "Gaurav Talekar - Services",
+      description: "Discover services offered by Gaurav Talekar",
+      url: `https://${data.domain}/services`,
+      images: [
+        {
+          url: "/images/gaurav-talekar.jpg",
+          width: 1080,
+          height: 1080,
+          alt: "Portrait of Gaurav Talekar"
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: "Gaurav Talekar - Services",
+      description: "Discover services offered by Gaurav Talekar",
+      images: ["/images/gaurav-talekar.jpg"],
+    },
+  }
+}
+
+const ServicesPage = () => {
+  return (
+    <>
+      <FadeInObserver />
+      <SectionComponent id="contact-section" aria-label="List of services offered by Gaurav Talekar" bgColor="bg-white">
+        <SectionHeader headingLevel="h1" title={"Services"} clx={"text-shark-800 font-montserrat font-bold"} />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          {services.map((service, idx) => (
+            <article
+              key={idx}
+              className="bg-white px-8 py-6 border rounded-xl hover:shadow-md fade-in transition-all duration-500 ease-out delay-200"
+              aria-labelledby={`service-title-${idx}`}
+            >
+              <div className="flex items-start space-x-4 mb-4">
+                <figure className="w-16 h-16 flex-shrink-0">
+                  <Image
+                    src={service.icon}
+                    alt={`${service.title} icon`}
+                    width={64}
+                    height={64}
+                    className="w-16 h-16"
+                  />
+                </figure>
+                <h2
+                  id={`service-title-${idx}`}
+                  className=" text-2xl md:text-3xl flex-grow font-montserrat font-semibold"
+                >
+                  {service.title}
+                </h2>
+              </div>
+
+              <ul className="font-nunito list-inside flex flex-wrap justify-start gap-x-4 mt-6">
+                {service.offering.map((item, subIdx) => (
+                  <li key={subIdx} className="flex items-center space-x-2 text-lg">
+                    {/* Decorative checkmark icon */}
+                    <svg
+                      className="shrink-0 w-3.5 h-3.5"
+                      aria-hidden="true"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        x="0.686295"
+                        y="12"
+                        width="16"
+                        height="16"
+                        transform="rotate(-45 0.686295 12)"
+                        fill="#EC2E90"
+                      />
+                    </svg>
+
+                    <span>{item}</span>
+                  </li>
+                ))}
+              </ul>
+              <p className="font-nunito text-lg mt-6">
+                {service.details}
+              </p>
+            </article>
+          ))}
+        </div>
+      </SectionComponent>
+      <SectionComponent id="contact-section" aria-label="Process followed by Gaurav Talekar" bgColor="bg-white">
+        <SectionHeader headingLevel="h1" title={"Process"} clx={"text-shark-800 font-montserrat font-bold"} />
+        <div className="mt-6">
+          <ul className="mx-auto grid grid-cols-1 gap-10 sm:mt-16 lg:grid-cols-4">
+            {
+              processes.map((process, idx) => (
+                <li className="flex-start group relative flex lg:flex-col" key={idx}>
+                  {idx !== processes.length - 1 && ( // To not render the span for last item
+                    <span
+                      className="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-auto lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
+                      aria-hidden="true"
+                    ></span>
+                  )}
+                  <div
+                    className="inline-flex h-14 w-14 shrink-0 items-center justify-center rounded-xl border border-gray-300 bg-gray-50 transition-all duration-200 group-hover:border-gray-900 group-hover:bg-gray-900">
+
+                    <Image
+                      src={process.icon}
+                      alt={"desgin"}
+                      height={512}
+                      width={512}
+                      className="h-12 w-12"
+                    />
+                  </div>
+                  <div className="ml-6 lg:ml-0 lg:mt-3">
+                    <h2
+                      className="text-xl font-bold text-gray-900 before:mb-2 before:block before:font-mono before:text-sm before:text-gray-500">
+                      {idx + 1}{". "}{process.title}
+                    </h2>
+                    <p className="mt-2 text-lg text-gray-700">{process.details}</p>
+                  </div>
+                </li>
+              ))
+            }
+
+          </ul>
+        </div>
+      </SectionComponent>
+    </>
+  )
+}
+
+export default ServicesPage
--- data/services.js
+++ data/services.js
@@ -3,20 +3,91 @@ export const services = [
     title: "BRANDING & IDENTITY SYSTEM",
     icon: "/images/logos/branding.png",
     offering: ["Logo & Identity", "Style Guide", "Packaging", "Art Direction", "Design Research"],
+    details: `
+    Building a brand visually always starts with a logo. 
+    When collaborating together we’ll hone in on a concept that is both eye-catching and represents the brand’s value and purpose. 
+    I can provide services from logo design, to a full identity system, which entails typography styles, colour palettes, brand elements, photography, etc. 
+    Along with developing application rules to how the identity would live in a physical and digital environment.
+    `
   },
   {
     title: "ILLUSTRATIONS & ICONOGRAPHY",
     icon: "/images/logos/illustration.png",
     offering: ["Editorial Illustrations", "Landscape Illustrations", "Character Design", "Infographics"],
+    details: `
+    From vector to raster illustration styles, I have the skills to produce a series of illustrations that 
+    have a very stand out and permanent use, such as book illustrations, packaging design, and poster art.
+    I develop illustrations and iconography sets that would serve as visual communication for a brand.
+    `
   },
   {
     title: "WEBSITE DESIGN",
     icon: "/images/logos/social-media.png",
     offering: ["Wireframing", "UI & UX Design", "Prototyping", "Development"],
+    details: `
+    If you are looking for a new website with a brand roll out, or need a refresher for your current website, 
+    I have experience working with custom website builders such as Webflow, and/or could provide design and set up sites, 
+    such as Shopify, SquareSpace or Wix.
+    `
   },
   {
     title: "SOCIAL MEDIA CONTENT",
     icon: "/images/logos/web-design.png",
     offering: ["Product Photography", "Retouch Photo Editing", "Social Campaign Strategy"],
+    details: `
+    With social media and branding it is always key to keep consistent engagement with your platforms. 
+    I provide guidance with developing a social media guideline, and creating content that is impactful and 
+    will help communicate the brand’s goals and services.
+    `
   },
-]
+];
+
+export const processes = [
+  {
+    title: "Research",
+    icon: "/images/logos/research.png",
+    details: `
+    Understanding project's vision, industry, and target audience. 
+    Along with that creating mood board that align with the theme of the project.
+    `
+  },
+  {
+    title: "Concept",
+    icon: "/images/logos/concept.png",
+    details: `
+    Creating 2-3 rough concepts in different directions and ideas based on the research.
+    `
+  },
+  {
+    title: "Sketching",
+    icon: "/images/logos/sketching.png",
+    details: `
+    Before heading into the digital realm, I start by sketching out ideas on paper. 
+    Focusing on the forms, structure, and composition.
+    `
+  },
+  {
+    title: "Design",
+    icon: "/images/logos/design.png",
+    details: `
+    The designing phase where the design takes shape with use of tools like Adobe Creative Suite to develop the initial drafts.
+    `
+  },
+  {
+    title: "Review",
+    icon: "/images/logos/review.png",
+    details: `
+    This phase ensures the design aligns with the project expectation decided in research phase. 
+    Gathering feedback and making revisions as needed.
+    `
+  },
+  {
+    title: "Layout",
+    icon: "/images/logos/layout.png",
+    details: `
+    The final step focuses on preparing the design for its intended purpose. 
+    Creating adaptable layouts for different formats e.g., web, print, social media. 
+    Delivering finalized files and assets, ready for implementation or production.
+    `
+  },
+];
