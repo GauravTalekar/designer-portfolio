3543cf53c240a679605f551a2ccc25717ddd44cd
GauravTalekar <gaurav.talekar.designer@gmail.com>
2025-06-06 00:47:52 +0200

    Hide Service and contact from home

--- app/page.js
+++ app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
       <HeroHeader />
       <AboutMe />
       <MasonryGridSection />
-      <ServicesSection />
-      <ContactSection />
+      {/* <ServicesSection />
+      <ContactSection /> */}
     </>
   );
 }
