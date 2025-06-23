4058c7a88d5a74b5c9729a8a3718f016dad25293
GauravTalekar <gaurav.talekar.designer@gmail.com>
2025-06-23 17:35:05 +0200

    comment Highlights fro about me

--- data/site-details.js
+++ data/site-details.js
@@ -32,14 +32,14 @@ export const socials = [
 export const about = [
   {
     text: "Gaurav Talekar, an illustrator, graphic and UX/UI-designer based in Bremen, Germany-based designer who was born in Mumbai, India.",
-    highlight: ["Bremen, Germany-based designer", "Mumbai, India"],
+    // highlight: ["Bremen, Germany-based designer", "Mumbai, India"],
   },
   {
     text: "His work oscillates between substantial usage of colors and illustrations, creating a playful and optimistic style. It always starts with paper sketches and culminates in the digital space. His work is an exploration of different forms of shapes and emphasizes the connotations behind color.",
   },
   {
     text: "Gaurav draws inspiration from his own cultural thread, using abundant colors to tell stories. He's passionate about typography, design, animation, and print. When he's not designing, you might find him in a ceramic studio, shaping clay.",
-    underline: ["typography, design, animation, and print"]
+    // underline: ["typography, design, animation, and print"]
   },
   {
     text: "He has worked as a Graphic and as a UX/UI-designer in India and in Germany.",
